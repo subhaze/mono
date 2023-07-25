5d8d01207ba695d90c65bb8a277d4e08b484911f
Michael Russell <jmrussell22@gmail.com>
2023-07-25 13:17:33 -0400

    [app-proxy] update index file for proxy to show possible domains if given domain is unknown

--- apps/app-proxy/index.js
+++ apps/app-proxy/index.js
@@ -6,20 +6,34 @@ const httpProxy = require("http-proxy");
 const appProxy = httpProxy.createProxyServer();
 
 const PORT = process.env.PORT ?? 8443;
-const BASE_DOMAIN = process.env.BASE_DOMAIN || '127local.host';
+const BASE_DOMAIN = process.env.BASE_DOMAIN || "127local.host";
 
 const appDomain = new Map();
 
-appDomain.set(`faulty-technology.${BASE_DOMAIN}:${PORT}`, `http://localhost:3000`);
-appDomain.set(`asocial-network.${BASE_DOMAIN}:${PORT}`, `http://localhost:3001`)
+appDomain.set(
+	`faulty-technology.${BASE_DOMAIN}:${PORT}`,
+	`http://localhost:3000`
+);
+appDomain.set(
+	`asocial-network.${BASE_DOMAIN}:${PORT}`,
+	`http://localhost:3001`
+);
 
 app.get("/*", function (req, res) {
 	const domain = appDomain.get(req.headers.host);
 	if (domain) {
 		appProxy.web(req, res, { target: domain });
 	} else {
-    res.send(`Domain unknown: ${domain}`)
-  }
+		res.send(`
+
+Domain unknown: ${req.headers.host}
+
+${Array.from(appDomain).map(
+	(a) => `<div><a href="https://${a[0]}">${a[0]} -> ${a[1]}</a></div>`
+).join('')}
+
+`);
+	}
 });
 https
 	.createServer(
