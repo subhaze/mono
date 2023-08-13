85c9ee78ff86277da2f609da3fb0bc8d348c078e
Michael Russell <jmrussell22@gmail.com>
2023-08-13 13:38:22 -0400

    [faulty-technology] updating styles/layout/copy

--- apps/faulty-technology/src/app/layout.tsx
+++ apps/faulty-technology/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
-// include styles from the ui package
-import "ui/styles.css";
+import Image from "next/image";
+import almondBlossoms from "../almond-blossom.jpg";
 
 export default function RootLayout({
 	children,
@@ -9,8 +9,14 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className="bg-slate-50 dark:bg-slate-950 px-6 lg:px-0">
+			<body className="bg-slate-50 px-6 dark:bg-slate-950 lg:px-0">
 				<div className="background absolute inset-0" />
+				<Image
+					alt=""
+					src={almondBlossoms.src}
+					className="absolute inset-0 object-cover opacity-60"
+					fill
+				/>
 				{children}
 			</body>
 		</html>
--- apps/faulty-technology/src/app/page.tsx
+++ apps/faulty-technology/src/app/page.tsx
@@ -1,14 +1,20 @@
 import { Metadata } from "next";
+import { CSSProperties } from "react";
 
 export const metadata: Metadata = {
-	title: "We're all Faulty Technology, and it's okay",
+	title: "We're all Faulty Technology, and it's okay.",
 };
 
 export default function Home() {
 	return (
-		<div className="relative my-8 w-full rounded-sm bg-slate-100 px-6 py-12 shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 dark:bg-slate-900 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pb-28 lg:pt-16">
-			<main className="prose dark:prose-invert prose-slate lg:prose-lg mx-auto">
-				<h1>Faulty Technology</h1>
+		<div className="relative my-8 w-full rounded-sm  bg-slate-100/40 px-6 py-12 shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 backdrop-blur dark:bg-slate-900/50 md:mx-auto md:max-w-3xl lg:max-w-4xl lg:pb-28 lg:pt-16">
+			<main className="prose dark:prose-invert prose-slate lg:prose-lg relative mx-auto">
+				<h1>
+					<span className="[-webkit-text-fill-color:rgba(0,0,0,0.1)] [-webkit-text-stroke-color:rgba(0,0,0,0.4)] [-webkit-text-stroke-width:1px] dark:[-webkit-text-fill-color:rgba(255,255,255,0.4)]">
+						Faulty
+					</span>{" "}
+					<span className="text-slate-600 dark:text-slate-400">Technology</span>
+				</h1>
 
 				<blockquote>
 					<p>Success is sometimes the outcome of a whole string of failures.</p>
@@ -17,9 +23,8 @@ export default function Home() {
 				</blockquote>
 
 				<p>
-					Don&apos;t fear failure, don&apos;t fear risk, in the end, we&apos;re
-					all <cite>Faulty Technology</cite>, embrace it, move forward, and
-					savor every moment.
+					Don&apos;t fear failure, in the end, we&apos;re all{" "}
+					<cite>Faulty Technology</cite>, embrace it, and move forward.
 				</p>
 			</main>
 		</div>
