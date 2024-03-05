8fb4264d4c1eac02f6301a029c6970b215f270c8
Michael Russell <jmrussell22@gmail.com>
2024-03-05 18:25:32 -0500

    new site, new you (#9)
    * new site, new you
    
    * update deps
    
    * trigger workflow on workflow updates for app
    
    * pin vercel version, there's a bug in latest

---
+++ apps/itsthecereal-life/src/app/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import trix from "../assets/life-cereal.webp";
+
+export const metadata: Metadata = {
+	title: "It's not the cereal life, so what is it?",
+};
+
+export default function Home() {
+	return (
+		<main className="">
+			<div className="h-screen bg-white p-6">
+				<Image
+					className="block h-full w-full bg-white object-contain"
+					src={trix.src}
+					alt="picture of Trix cereal box"
+					width={trix.width}
+					height={trix.height}
+				/>
+				<p className="text-gray-500">Mikey likes it!</p>
+			</div>
+		</main>
+	);
+}
